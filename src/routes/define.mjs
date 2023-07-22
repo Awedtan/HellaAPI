35ea48103c908a6a4543301a9ff5e182078840ad
Awedtan <31903483+Awedtan@users.noreply.github.com>
2023-07-21 22:04:56 -0400

    add define get all

--- src/routes/define.mjs
+++ src/routes/define.mjs
@@ -3,6 +3,14 @@ import db from "../db.mjs";
 
 const defineRouter = express.Router();
 
+defineRouter.get("", async (req, res) => {
+    let collection = await db.collection("definitions");
+    let result = await collection.find().toArray();
+
+    if (!result) res.send("Not found").status(404);
+    else res.send(result).status(200);
+});
+
 defineRouter.get("/:id", async (req, res) => {
     let collection = await db.collection("definitions");
     let result = await collection.findOne({ keys: req.params.id });
